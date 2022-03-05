1cccfb068f3291c73a075018cd2eb6fad8950092
HauleTZ <williamhaule326.com>
2022-03-05 15:21:09 +0000

    added question and choice inline

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import *
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 3
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2    
 
 
 class LessonInline(admin.StackedInline):
@@ -21,10 +30,14 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
-
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
 # <HINT> Register Question and Choice models here
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -128,6 +128,7 @@ class Question(models.Model):
     
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=30)
     is_correct = models.BooleanField()
 
 # <HINT> The submission model
